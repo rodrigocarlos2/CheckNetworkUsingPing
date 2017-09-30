112b900e9615968a6e4831434c33a2f6513df2da
Rodrigo Carlos Carvalho Lima Barbosa Leal <rodrigo19962010@live.com>
2017-09-30 15:21:39 -0300

    Create Source.py

---
+++ Source.py
@@ -0,0 +1,16 @@
+
+
+import os
+
+def check_ping():
+    hostname = "google.com"
+    response = os.system("ping -c 1 " + hostname)
+    # and then check the response...
+    if response == 0:
+        pingstatus = "Network Active"
+    else:
+        pingstatus = "Network Error"
+
+    return pingstatus
+
+print check_ping()
